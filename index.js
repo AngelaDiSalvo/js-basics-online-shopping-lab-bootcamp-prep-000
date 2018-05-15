4a8f8182ab6f85d52a7b076a9770a5d3292e7cbb
Angela DiSalvo <angela.disalvo@gmail.com>
2018-05-15 21:36:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  card = parseInt(cardNumber)
+  var card = parseInt(cardNumber)
   if (card === null) {
     return "Sorry, we don't have a credit card on file for you."
   } 
